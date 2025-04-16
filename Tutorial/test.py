79ccfdba9d087cfc7a5126e6045381ad378d67e0
CliffKai <cliffkai@CliffKaideMacBook-Air.local>
2025-04-16 10:29:46 +0800

    2025-4-16

--- Tutorial/test.py
+++ Tutorial/test.py
@@ -2,3 +2,14 @@ import torch
 print(torch.hub.get_dir())
 import os
 print(os.getenv('TORCH_HOME'))
+
+import torch
+import torchvision
+from torch import nn
+from collections import OrderedDict
+
+# 通过注释此行代码来观察方法1的缺陷
+# from model_save import *
+
+tudui_method1 = torch.load("../Models/Tudui/tudui_method1.pth")
+print(tudui_method1)

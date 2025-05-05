4273c404dacc4aef223e2837d59280b72bc94d28
CliffKai <kkwei0819@foxmail.com>
2025-05-05 22:13:29 +0800

    2025-5-5

--- Tutorial/ViT/test.py
+++ Tutorial/ViT/test.py
@@ -1,6 +1,8 @@
 import torch
-from simple_vit import SimpleViT  # 来自 simple_vit 包
 
 h = 4
 w = 3
-torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+x, y = torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+
+print(x)
+print(y)

cbdea740279f13d9bb81c4fdf8c3b50561e3bc53
CliffKai <kkwei0819@foxmail.com>
2025-05-06 10:05:07 +0800

    2025-5-6

--- Tutorial/ViT/test.py
+++ Tutorial/ViT/test.py
@@ -1,8 +1,12 @@
-import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
 
-h = 4
-w = 3
-x, y = torch.meshgrid(torch.arange(h), torch.arange(w), indexing="ij")
+train_dataset = datasets.CIFAR10(root='../../datasets/CIFAR10', train=True, download=True, transform=transforms.ToTensor())
+test_dataset  = datasets.CIFAR10(root='../../datasets/CIFAR10', train=False, download=True, transform=transforms.ToTensor())
 
-print(x)
-print(y)
+train_data = DataLoader(dataset = train_dataset, batch_size = 1, shuffle = True, num_workers = 0, drop_last = False)
+
+for datas in train_data:
+    img, label = datas
+    print(img.shape)

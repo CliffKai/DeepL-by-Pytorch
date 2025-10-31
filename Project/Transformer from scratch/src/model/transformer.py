02bbdc02b3bcb77f9c7abd6fe5c37d34780c0e3f
CliffKai <cliffkai@CliffKaideMacBook-Air.local>
2025-10-31 19:57:11 +0800

    update Transformer from scratch(In progress)

--- Project/Transformer from scratch/src/model/transformer.py
+++ Project/Transformer from scratch/src/model/transformer.py
@@ -4,11 +4,11 @@ import torch.nn as nn
 from torch import Tensor
 from jaxtyping import Float, Int
 
-from src.model.RMSNorm import RMSNorm
-from src.model.attention import MultiHeadSelfAttention
-from src.model.FFN import SwiGLU
-from src.model.Embedding import RotaryPositionalEmbedding, Embedding
-from src.model.Linear import Linear
+from .RMSNorm import RMSNorm
+from .attention import MultiHeadSelfAttention
+from .FFN import SwiGLU
+from .Embedding import RotaryPositionalEmbedding, Embedding
+from .Linear import Linear
 
 class TransformerBlock(nn.Module):
     def __init__(
@@ -26,19 +26,19 @@ class TransformerBlock(nn.Module):
         self.attn = MultiHeadSelfAttention(d_model, num_heads, rope, **factory_kwargs)
         self.ln2 = RMSNorm(d_model, **factory_kwargs)
         self.ffn = SwiGLU(d_model, d_ff, **factory_kwargs)
-
+    
     def forward(
         self,
-        x: Float[Tensor, "batch seq_len d_model"],
-        token_positions: Int[Tensor, "batch seq_len"],
+        x: Float[Tensor, "... batch seq_len d_model"],
+        token_positions: Int[Tensor, "batch seq_len"]
     ) -> Float[Tensor, "batch seq_len d_model"]:
         residual = x
-        x_norm = self.ln1(x)
+        x_norm = self.ln(x)
         attn_out = self.attn(x_norm, token_positions)
         x = residual + attn_out
 
         residual = x
-        x_norm = self.ln2(x)
+        x_norm = self.ln(x)
         ffn_out = self.ffn(x_norm)
         x = residual + ffn_out
 
@@ -60,9 +60,8 @@ class TransformerLM(nn.Module):
         super().__init__()
         factory_kwargs = {"device": device, "dtype": dtype}
         d_head = d_model // num_heads
-        self.token_embedding = Embedding(vocab_size, d_model, **factory_kwargs)
+        self.token_embeddings = Embedding(vocab_size, d_model, **factory_kwargs)
         rope = RotaryPositionalEmbedding(d_head, context_length, rope_theta, device=device)
-
         self.layers = nn.ModuleList([
             TransformerBlock(d_model, num_heads, d_ff, rope, **factory_kwargs)
             for _ in range(num_layers)
@@ -76,10 +75,8 @@ class TransformerLM(nn.Module):
     ) -> Float[Tensor, "batch seq_len vocab_size"]:
         batch_size, seq_len = in_indices.shape
         device = in_indices.device
-
         token_positions = torch.arange(seq_len, device=device).unsqueeze(0).expand(batch_size, -1)
-
-        x = self.token_embedding(in_indices)
+        x = self.token_embeddings(in_indices)
         for layer in self.layers:
             x = layer(x, token_positions)
         x = self.ln_final(x)

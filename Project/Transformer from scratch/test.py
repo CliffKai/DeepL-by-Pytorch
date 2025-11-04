16e86327eb7e22468e0e79898409494309e52ff9
CliffKai <cliffkai@CliffKaideMacBook-Air.local>
2025-11-04 22:57:56 +0800

    update Transformer from scratch(In progress)

--- Project/Transformer from scratch/test.py
+++ Project/Transformer from scratch/test.py
@@ -1,87 +1,116 @@
+from __future__ import annotations
+
+import math
 import torch
 import torch.nn as nn
 
-from torch import Tensor
-from jaxtyping import Float, Int
+from typing import Iterable, Optional
+from torch.optim import Optimizer
 
-from src.model.RMSNorm import RMSNorm
-from src.model.attention import MultiHeadSelfAttention
-from src.model.FFN import SwiGLU
-from src.model.Embedding import RotaryPositionalEmbedding, Embedding
-from src.model.Linear import Linear
-
-class TransformerBlock(nn.Module):
+class AdamWCustom(Optimizer):
     def __init__(
         self,
-        d_model: int,
-        num_heads: int,
-        d_ff: int,
-        rope: RotaryPositionalEmbedding,
-        device: torch.device | str | None=None,
-        dtype: torch.dtype | None=None,
+        params: Iterable[nn.Parameter],
+        lr: float = 1e-3,
+        betas: tuple[float, float] = (0.9, 0.999),
+        eps: float = 1e-8,
+        weight_decay: float = 0.01,
     ):
-        super().__init__()
-        factory_kwargs = {"device": device, "dtype": dtype}
-        self.ln1 = RMSNorm(d_model, **factory_kwargs)
-        self.attn = MultiHeadSelfAttention(d_model, num_heads, rope, **factory_kwargs)
-        self.ln2 = RMSNorm(d_model, **factory_kwargs)
-        self.ffn = SwiGLU(d_model, d_ff, **factory_kwargs)
+        if lr < 0.0:
+            raise ValueError(f"Invalid lr: {lr}")
+        if not 0.0 <= betas[0] < 1.0:
+            raise ValueError(f"Invalid beta1: {betas[0]}")
+        if not 0.0 <= betas[1] < 1.0:
+            raise ValueError(f"Invalid beta2: {betas[1]}")
+        if eps <= 0.0:
+            raise ValueError(f"Invalid eps: {eps}")
+        if weight_decay < 0.0:
+            raise ValueError(f"Invalid weight_decay: {weight_decay}")
+        
+        defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
+        super().__init__(params, defaults)
 
-    def forward(
-        self,
-        x: Float[Tensor, "batch seq_len d_model"],
-        token_positions: Int[Tensor, "batch seq_len"],
-    ) -> Float[Tensor, "batch seq_len d_model"]:
-        residual = x
-        x_norm = self.ln1(x)
-        attn_out = self.attn(x_norm, token_positions)
-        x = residual + attn_out
+    @torch.no_grad()
+    def forward(self,closure: Optional[callable] = None):
+        loss = None
+        if closure is not None:
+            with torch.enable_grad():
+                loss = closure()
+        
+        for group in self.param_groups:
+            lr: float = group["lr"]
+            beta1, beta2 = group["betas"]
+            eps: float = group["eps"]
+            weight_decay: float = group["weight_decay"]
 
-        residual = x
-        x_norm = self.ln2(x)
-        ffn_out = self.ffn(x_norm)
-        x = residual + ffn_out
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                grad = p.grad
 
-        return x
+                if grad.is_sparse:
+                    raise RuntimeError("AdamWCustom dose not support sparse gradients")
+                
+                state = self.state[p]
+                if len(state) == 0:
+                    state["step"] = 0
+                    state["exp_avg"] = torch.zeros_like(p, memory_format=torch.preserve_format)
+                    state["exp_avg_sq"] = torch.zeros_like(p, memory_format=torch.preserve_format)
+
+                exp_avg: torch.Tensor = state["exp_avg"]
+                exp_avg_sq: torch.Tensor = state["exp_avg_sq"]
+
+                state["step"] += 1
+                step: int = state["step"]
+
+                if weight_decay != 0.0:
+                    p.add_(p, alpha=-lr * weight_decay)
+                
+                exp_avg.mul_(beta1).add_(grad, alpha=1 - beta1)
+                exp_avg_sq.mul_(beta2).addcmul_(grad, grad, value=1 - beta2)
+
+                bias_correction1 = 1.0 - beta1 ** step
+                bias_correction2 = 1.0 - beta2 ** step
+
+                step_size = lr / bias_correction1
+
+                denom = (exp_avg_sq.sqrt() / math.sqrt(bias_correction2)).add_(eps)
+
+                p.addcdiv_(exp_avg, denom, value=-step_size)
+
+        return loss
     
-class TransformerLM(nn.Module):
-    def __init__(
-        self,
-        vocab_size: int,
-        context_length: int,
-        d_model: int,
-        num_layers: int,
-        num_heads: int,
-        d_ff: int,
-        rope_theta: float,
-        device: torch.device | str | None=None,
-        dtype: torch.dtype | None=None,
-    ):
-        super().__init__()
-        factory_kwargs = {"device": device, "dtype": dtype}
-        d_head = d_model // num_heads
-        self.token_embedding = Embedding(vocab_size, d_model, **factory_kwargs)
-        rope = RotaryPositionalEmbedding(d_head, context_length, rope_theta, device=device)
+def get_adamw_cls():
+    return AdamWCustom
 
-        self.layers = nn.ModuleList([
-            TransformerBlock(d_model, num_heads, d_ff, rope, **factory_kwargs)
-            for _ in range(num_layers)
-        ])
-        self.ln_final = RMSNorm(d_model, **factory_kwargs)
-        self.lm_head = Linear(d_model, vocab_size, **factory_kwargs)
+def get_lr_cosine_schedule(
+    *,
+    it: int,
+    max_learning_rate: float,
+    min_learning_rate: float,
+    warmup_iters: int,
+    cosint_cycle_iters: int,
+) -> float:
+    if it <= warmup_iters:
+        if warmup_iters <= 0:
+            return float(max_learning_rate)
+        return float(max_learning_rate) * (it / float(warmup_iters))
+    
+    span = max(1, cosint_cycle_iters - warmup_iters)
+    progress = (it - warmup_iters) / float(span)
+    if progress >= 1.0:
+        return float(min_learning_rate)
+    
+    cosine = 0.5 * (1.0 + math.cos(math.pi * progress))
+    lr = min_learning_rate + (max_learning_rate - min_learning_rate) * cosine
 
-    def forward(
-        self,
-        in_indices: Int[Tensor, "batch seq_len"],
-    ) -> Float[Tensor, "batch seq_len vocab_size"]:
-        batch_size, seq_len = in_indices.shape
-        device = in_indices.device
+    if lr < min_learning_rate:
+        lr = min_learning_rate
+    if lr > max_learning_rate:
+        lr =max_learning_rate
+    return float(lr)
+    
 
-        token_positions = torch.arange(seq_len, device=device).unsqueeze(0).expand(batch_size, -1)
 
-        x = self.token_embedding(in_indices)
-        for layer in self.layers:
-            x = layer(x, token_positions)
-        x = self.ln_final(x)
-        logits = self.lm_head(x)
-        return logits
+
+        
